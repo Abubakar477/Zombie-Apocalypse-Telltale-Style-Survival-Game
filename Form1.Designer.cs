cb1c608fb98e6769a48f26d2399f9b473826694f
Abubakar <abubakar2pp@gmail.com>
2026-03-28 11:03:01 -0700

    icon update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -55,103 +55,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panelContent = new System.Windows.Forms.Panel();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.panelOptions = new System.Windows.Forms.Panel();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.btnOption1 = new System.Windows.Forms.Button();
+            this.btnOption2 = new System.Windows.Forms.Button();
+            this.btnOption3 = new System.Windows.Forms.Button();
+            this.btnOption4 = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
             this.panelStart = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
-            this.rtbOutput = new System.Windows.Forms.RichTextBox();
-            this.panelOptions = new System.Windows.Forms.Panel();
-            this.btnOption4 = new System.Windows.Forms.Button();
-            this.btnOption3 = new System.Windows.Forms.Button();
-            this.btnOption2 = new System.Windows.Forms.Button();
-            this.btnOption1 = new System.Windows.Forms.Button();
             this.panelStatus = new System.Windows.Forms.Panel();
             this.pbScene = new System.Windows.Forms.PictureBox();
             this.lblChapter = new System.Windows.Forms.Label();
-            this.lblInventory = new System.Windows.Forms.Label();
-            this.lstInventory = new System.Windows.Forms.ListBox();
             this.lblHealth = new System.Windows.Forms.Label();
             this.pbHealth = new System.Windows.Forms.ProgressBar();
-            this.btnRestart = new System.Windows.Forms.Button();
-            this.btnContinue = new System.Windows.Forms.Button();
-
-            // --- NAYE BUTTONS INITIALIZE KIYE HAIN ---
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnLoad = new System.Windows.Forms.Button();
-            // -----------------------------------------
-
+            this.lblInventory = new System.Windows.Forms.Label();
+            this.lstInventory = new System.Windows.Forms.ListBox();
+            this.panelContent.SuspendLayout();
+            this.panelOptions.SuspendLayout();
+            this.panelStart.SuspendLayout();
+            this.panelStatus.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbScene)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelContent
+            // 
+            this.panelContent.Controls.Add(this.rtbOutput);
+            this.panelContent.Controls.Add(this.panelOptions);
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(200, 0);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(600, 450);
+            this.panelContent.TabIndex = 9;
             // 
             // rtbOutput
             // 
+            this.rtbOutput.BackColor = System.Drawing.Color.Black;
             this.rtbOutput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rtbOutput.Location = new System.Drawing.Point(200, 0);
+            this.rtbOutput.Font = new System.Drawing.Font("Consolas", 10F);
+            this.rtbOutput.ForeColor = System.Drawing.Color.LightGreen;
+            this.rtbOutput.Location = new System.Drawing.Point(0, 0);
             this.rtbOutput.Name = "rtbOutput";
             this.rtbOutput.ReadOnly = true;
-            this.rtbOutput.BackColor = System.Drawing.Color.Black;
-            this.rtbOutput.ForeColor = System.Drawing.Color.LightGreen;
-            this.rtbOutput.Font = new System.Drawing.Font("Consolas", 10F);
             this.rtbOutput.Size = new System.Drawing.Size(600, 340);
             this.rtbOutput.TabIndex = 0;
             this.rtbOutput.Text = "";
             // 
             // panelOptions
             // 
+            this.panelOptions.Controls.Add(this.btnContinue);
+            this.panelOptions.Controls.Add(this.btnOption1);
+            this.panelOptions.Controls.Add(this.btnOption2);
+            this.panelOptions.Controls.Add(this.btnOption3);
+            this.panelOptions.Controls.Add(this.btnOption4);
+            this.panelOptions.Controls.Add(this.btnRestart);
+            this.panelOptions.Controls.Add(this.btnSave);
+            this.panelOptions.Controls.Add(this.btnLoad);
             this.panelOptions.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panelOptions.Location = new System.Drawing.Point(0, 340);
             this.panelOptions.Name = "panelOptions";
             this.panelOptions.Size = new System.Drawing.Size(600, 110);
             this.panelOptions.TabIndex = 1;
-            // 
-            // btnOption4
-            // 
-            this.btnOption4.Location = new System.Drawing.Point(610, 30);
-            this.btnOption4.Name = "btnOption4";
-            this.btnOption4.Size = new System.Drawing.Size(140, 40);
-            this.btnOption4.TabIndex = 4;
-            this.btnOption4.Text = "Option 4";
-            this.btnOption4.UseVisualStyleBackColor = true;
-            this.btnOption4.Click += new System.EventHandler(this.btnOption4_Click);
-            // 
-            // btnOption3
-            // 
-            this.btnOption3.Location = new System.Drawing.Point(410, 30);
-            this.btnOption3.Name = "btnOption3";
-            this.btnOption3.Size = new System.Drawing.Size(140, 40);
-            this.btnOption3.TabIndex = 3;
-            this.btnOption3.Text = "Option 3";
-            this.btnOption3.UseVisualStyleBackColor = true;
-            this.btnOption3.Click += new System.EventHandler(this.btnOption3_Click);
-            // 
-            // btnOption2
-            // 
-            this.btnOption2.Location = new System.Drawing.Point(210, 30);
-            this.btnOption2.Name = "btnOption2";
-            this.btnOption2.Size = new System.Drawing.Size(140, 40);
-            this.btnOption2.TabIndex = 2;
-            this.btnOption2.Text = "Option 2";
-            this.btnOption2.UseVisualStyleBackColor = true;
-            this.btnOption2.Click += new System.EventHandler(this.btnOption2_Click);
-            // 
-            // btnOption1
-            // 
-            this.btnOption1.Location = new System.Drawing.Point(10, 30);
-            this.btnOption1.Name = "btnOption1";
-            this.btnOption1.Size = new System.Drawing.Size(140, 40);
-            this.btnOption1.TabIndex = 1;
-            this.btnOption1.Text = "Option 1";
-            this.btnOption1.UseVisualStyleBackColor = true;
-            this.btnOption1.Click += new System.EventHandler(this.btnOption1_Click);
-            // 
-            // btnRestart
-            // 
-            this.btnRestart.Location = new System.Drawing.Point(470, 30);
-            this.btnRestart.Name = "btnRestart";
-            this.btnRestart.Size = new System.Drawing.Size(110, 40);
-            this.btnRestart.TabIndex = 5;
-            this.btnRestart.Text = "Restart";
-            this.btnRestart.UseVisualStyleBackColor = true;
-            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
             // 
             // btnContinue
             // 
@@ -163,9 +133,59 @@
             this.btnContinue.UseVisualStyleBackColor = true;
             this.btnContinue.Visible = false;
             this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
-
-            // --- NAYE BUTTONS KI PROPERTIES (Save/Load) ---
+            // 
+            // btnOption1
+            // 
+            this.btnOption1.Location = new System.Drawing.Point(10, 30);
+            this.btnOption1.Name = "btnOption1";
+            this.btnOption1.Size = new System.Drawing.Size(140, 40);
+            this.btnOption1.TabIndex = 1;
+            this.btnOption1.Text = "Option 1";
+            this.btnOption1.UseVisualStyleBackColor = true;
+            this.btnOption1.Click += new System.EventHandler(this.btnOption1_Click);
+            // 
+            // btnOption2
+            // 
+            this.btnOption2.Location = new System.Drawing.Point(210, 30);
+            this.btnOption2.Name = "btnOption2";
+            this.btnOption2.Size = new System.Drawing.Size(140, 40);
+            this.btnOption2.TabIndex = 2;
+            this.btnOption2.Text = "Option 2";
+            this.btnOption2.UseVisualStyleBackColor = true;
+            this.btnOption2.Click += new System.EventHandler(this.btnOption2_Click);
+            // 
+            // btnOption3
+            // 
+            this.btnOption3.Location = new System.Drawing.Point(410, 30);
+            this.btnOption3.Name = "btnOption3";
+            this.btnOption3.Size = new System.Drawing.Size(140, 40);
+            this.btnOption3.TabIndex = 3;
+            this.btnOption3.Text = "Option 3";
+            this.btnOption3.UseVisualStyleBackColor = true;
+            this.btnOption3.Click += new System.EventHandler(this.btnOption3_Click);
+            // 
+            // btnOption4
+            // 
+            this.btnOption4.Location = new System.Drawing.Point(610, 30);
+            this.btnOption4.Name = "btnOption4";
+            this.btnOption4.Size = new System.Drawing.Size(140, 40);
+            this.btnOption4.TabIndex = 4;
+            this.btnOption4.Text = "Option 4";
+            this.btnOption4.UseVisualStyleBackColor = true;
+            this.btnOption4.Click += new System.EventHandler(this.btnOption4_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(470, 30);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(110, 40);
+            this.btnRestart.TabIndex = 5;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
             // btnSave
+            // 
             this.btnSave.Location = new System.Drawing.Point(10, 75);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(140, 30);
@@ -173,8 +193,9 @@
             this.btnSave.Text = "Save Game";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-
+            // 
             // btnLoad
+            // 
             this.btnLoad.Location = new System.Drawing.Point(160, 75);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(140, 30);
@@ -182,26 +203,48 @@
             this.btnLoad.Text = "Load Game";
             this.btnLoad.UseVisualStyleBackColor = true;
             this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
-            // ----------------------------------------------
-
             // 
-            // panelOptions Controls
+            // panelStart
             // 
-            this.panelOptions.Controls.Add(this.btnContinue);
-            this.panelOptions.Controls.Add(this.btnOption1);
-            this.panelOptions.Controls.Add(this.btnOption2);
-            this.panelOptions.Controls.Add(this.btnOption3);
-            this.panelOptions.Controls.Add(this.btnOption4);
-            this.panelOptions.Controls.Add(this.btnRestart);
-
-            // --- NAYE BUTTONS PANEL MEIN DAAL DIYE ---
-            this.panelOptions.Controls.Add(this.btnSave);
-            this.panelOptions.Controls.Add(this.btnLoad);
-            // -----------------------------------------
-
+            this.panelStart.BackColor = System.Drawing.Color.Black;
+            this.panelStart.Controls.Add(this.lblTitle);
+            this.panelStart.Controls.Add(this.btnStart);
+            this.panelStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelStart.Location = new System.Drawing.Point(200, 0);
+            this.panelStart.Name = "panelStart";
+            this.panelStart.Size = new System.Drawing.Size(600, 450);
+            this.panelStart.TabIndex = 10;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(600, 120);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "ZOMBIE APOCALYPSE: SURVIVAL";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStart.Location = new System.Drawing.Point(230, 180);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(140, 40);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // panelStatus
             // 
+            this.panelStatus.Controls.Add(this.pbScene);
+            this.panelStatus.Controls.Add(this.lblChapter);
+            this.panelStatus.Controls.Add(this.lblHealth);
+            this.panelStatus.Controls.Add(this.pbHealth);
+            this.panelStatus.Controls.Add(this.lblInventory);
+            this.panelStatus.Controls.Add(this.lstInventory);
             this.panelStatus.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelStatus.Location = new System.Drawing.Point(0, 0);
             this.panelStatus.Name = "panelStatus";
@@ -222,7 +265,7 @@
             this.lblChapter.AutoSize = true;
             this.lblChapter.Location = new System.Drawing.Point(10, 140);
             this.lblChapter.Name = "lblChapter";
-            this.lblChapter.Size = new System.Drawing.Size(92, 17);
+            this.lblChapter.Size = new System.Drawing.Size(67, 16);
             this.lblChapter.TabIndex = 1;
             this.lblChapter.Text = "Chapter: 1";
             // 
@@ -231,7 +274,7 @@
             this.lblHealth.AutoSize = true;
             this.lblHealth.Location = new System.Drawing.Point(10, 170);
             this.lblHealth.Name = "lblHealth";
-            this.lblHealth.Size = new System.Drawing.Size(53, 17);
+            this.lblHealth.Size = new System.Drawing.Size(46, 16);
             this.lblHealth.TabIndex = 2;
             this.lblHealth.Text = "Health";
             // 
@@ -241,7 +284,6 @@
             this.pbHealth.Name = "pbHealth";
             this.pbHealth.Size = new System.Drawing.Size(180, 23);
             this.pbHealth.TabIndex = 3;
-            this.pbHealth.Maximum = 100;
             this.pbHealth.Value = 100;
             // 
             // lblInventory
@@ -249,7 +291,7 @@
             this.lblInventory.AutoSize = true;
             this.lblInventory.Location = new System.Drawing.Point(10, 220);
             this.lblInventory.Name = "lblInventory";
-            this.lblInventory.Size = new System.Drawing.Size(67, 17);
+            this.lblInventory.Size = new System.Drawing.Size(61, 16);
             this.lblInventory.TabIndex = 4;
             this.lblInventory.Text = "Inventory";
             // 
@@ -262,68 +304,23 @@
             this.lstInventory.Size = new System.Drawing.Size(180, 164);
             this.lstInventory.TabIndex = 5;
             // 
-            // panelStatus Controls
-            // 
-            this.panelStatus.Controls.Add(this.pbScene);
-            this.panelStatus.Controls.Add(this.lblChapter);
-            this.panelStatus.Controls.Add(this.lblHealth);
-            this.panelStatus.Controls.Add(this.pbHealth);
-            this.panelStatus.Controls.Add(this.lblInventory);
-            this.panelStatus.Controls.Add(this.lstInventory);
-            // 
-            // panelContent
-            // 
-            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(200, 0);
-            this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(600, 450);
-            this.panelContent.TabIndex = 9;
-
-            // Form1
-            // panelStart
-            // 
-            this.panelStart.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelStart.BackColor = System.Drawing.Color.Black;
-            this.panelStart.Location = new System.Drawing.Point(200, 0);
-            this.panelStart.Name = "panelStart";
-            this.panelStart.Size = new System.Drawing.Size(600, 450);
-            this.panelStart.TabIndex = 10;
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = false;
-            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblTitle.Height = 120;
-            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.ForeColor = System.Drawing.Color.OrangeRed;
-            this.lblTitle.Text = "ZOMBIE APOCALYPSE: SURVIVAL";
-            // 
-            // btnStart
-            // 
-            this.btnStart.Text = "Start Game";
-            this.btnStart.Width = 140;
-            this.btnStart.Height = 40;
-            this.btnStart.Left = (this.panelStart.Width - this.btnStart.Width) / 2;
-            this.btnStart.Top = 180;
-            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            this.panelStart.Controls.Add(this.lblTitle);
-            this.panelStart.Controls.Add(this.btnStart);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            // Add rtbOutput and options into panelContent
-            this.panelContent.Controls.Add(this.rtbOutput);
-            this.panelContent.Controls.Add(this.panelOptions);
             this.Controls.Add(this.panelContent);
             this.Controls.Add(this.panelStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Zombie: Telltale";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelContent.ResumeLayout(false);
+            this.panelOptions.ResumeLayout(false);
+            this.panelStart.ResumeLayout(false);
+            this.panelStatus.ResumeLayout(false);
+            this.panelStatus.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbScene)).EndInit();
             this.ResumeLayout(false);
 
         }
